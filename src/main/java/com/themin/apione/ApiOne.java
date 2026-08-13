2f9e7b9a834e488854a0df41f684795c56cbc18b
shrikant <shri1824@gmail.com>
2026-08-13 12:24:03 +0200

    initial commit

---
+++ src/main/java/com/themin/apione/ApiOne.java
@@ -0,0 +1,13 @@
+package com.themin.apione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOne {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiOne.class, args);
+	}
+
+}
